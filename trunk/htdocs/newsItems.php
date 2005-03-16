a8d912c0daff9ccbd0b7624ad782a878f009e16a
rwareham <rwareham@35bde186-58f2-0310-a45f-d094064e9b28>
2005-03-16 11:41:17 +0000

    Clarify first news item.

--- trunk/htdocs/newsItems.php
+++ trunk/htdocs/newsItems.php
@@ -3,7 +3,7 @@ global $newsItems;
 
 $newsItems = array(
   array( 'date' => '15-Mar-05', 'title' => 'First Public Release',
-          'contents' => '<p>The first public release of XinePlayer has been made!</p>'),
+          'contents' => '<p>The first public release of XinePlayer has been made! Currently it is source only until I sort out some issues with my build system.</p>'),
 /*
   array( 'date' => '4-Jul-04', 'title' => 'Rich Wareham Interview',
 	  'contents' => '
