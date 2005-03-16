12a04387a01f6ed1a17d6e2b147f69d766b204b8
rwareham <rwareham@35bde186-58f2-0310-a45f-d094064e9b28>
2005-03-16 13:05:13 +0000

    Fix install name for xine-lib in plugins

--- trunk/XinePlayer/XinePlayer/FixInstallName.sh
+++ trunk/XinePlayer/XinePlayer/FixInstallName.sh
@@ -8,10 +8,19 @@
 # Fix the install path of the xine dylib so that we can embed it in our framework.
 
 cd $BUILD_ROOT/$EXECUTABLE_FOLDER_PATH
-otool -L $EXECUTABLE_NAME
+#otool -L $EXECUTABLE_NAME
 NAME=`otool -L $EXECUTABLE_NAME | grep "xine.*\.dylib " | cut -f 2 | cut -f 1 -d ' '`
 NEWNAME=`basename $NAME`
-echo Changing $NAME ... 
+echo Changing $NAME in $EXECUTABLE_NAME ... 
 cp $NAME ../Resources/$NEWNAME
 install_name_tool -change $NAME @executable_path/../Resources/$NEWNAME $EXECUTABLE_NAME
-otool -L $EXECUTABLE_NAME
+
+cd $BUILD_ROOT/$EXECUTABLE_FOLDER_PATH/../PlugIns/
+for EXECUTABLE_NAME in `find . -name '*.so'`; do
+NAME=`otool -L $EXECUTABLE_NAME | grep "xine.*\.dylib " | cut -f 2 | cut -f 1 -d ' '`
+NEWNAME=`basename $NAME`
+echo Changing $NAME in $EXECUTABLE_NAME ... 
+install_name_tool -change $NAME @executable_path/../Resources/$NEWNAME $EXECUTABLE_NAME
+done
+
+#otool -L $EXECUTABLE_NAME
