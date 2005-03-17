8e948135db87fe839700069b344a37e7bb2ce48a
rwareham <rwareham@35bde186-58f2-0310-a45f-d094064e9b28>
2005-03-17 11:09:40 +0000

    released some builds!

--- trunk/htdocs/newsItems.php
+++ trunk/htdocs/newsItems.php
@@ -2,6 +2,8 @@
 global $newsItems;
 
 $newsItems = array(
+  array( 'date' => '17-Mar-05', 'title' => 'First Public Builds',
+          'contents' => '<p>I\'ve uploaded the first G3 and G4 builds to the <a href="http://developer.berlios.de/project/showfiles.php?group_id=3329">BerliOS file release system</a>. Please download the appropriate build for your processor. Also this is very, very alpha so I appologise for any bugs.</p>'),
   array( 'date' => '15-Mar-05', 'title' => 'First Public Release',
           'contents' => '<p>The first public release of XinePlayer has been made! Currently it is source only until I sort out some issues with my build system.</p>'),
 /*
